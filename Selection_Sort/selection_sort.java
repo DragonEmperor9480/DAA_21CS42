6dc7845d0a5dba2f6109c6f2f23cfd959d6f70ce
Amrutesh <amruteshnaregal1234@gmail.com>
2023-06-10 22:41:24 +0530

    selection sort in java

--- Selection_Sort/selection_sort.java
+++ Selection_Sort/selection_sort.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class selection_sort {
     public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
+    Random ran = new Random();
     int a,i,j,min,temp;
     System.out.println("Enter the number of elements in array");
     a = scn.nextInt();
     int b[] = new int[a];
-    System.out.println("Enter the Array elements");
-    for(i = 0; i < a; i++)
-    b[i] = scn.nextInt();
+    int c[] = new int[a];
+    for(i = 0; i < a; i++){
+    b[i] = ran.nextInt(5000);
+    c[i] = b[i];
+    }
      for( i = 0; i < a - 1; i++)
     {
     min = i;
@@ -23,6 +27,10 @@ public class selection_sort {
         b[min] = b[i];
         b[i] = temp;
     }
+    System.out.println("The Array before sorting is:");
+        for(i = 0; i < a; i++)
+        System.out.print(c[i] + " ");
+        System.out.println("\n");
     System.out.println("The Sorted Array is:");
         for(i = 0; i < a; i++)
         System.out.print(b[i] + " ");
